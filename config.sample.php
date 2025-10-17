a0e2b411f3a2f7072be413edb54891139940225b
PlakScooters <88717693+PlakScooters@users.noreply.github.com>
2025-10-18 01:36:34 +0300

    Add files via upload

---
+++ config.sample.php
@@ -0,0 +1,14 @@
+<?php
+// Переименуйте в config.php и заполните своими параметрами
+return [
+    'db' => [
+        'host' => '127.0.0.1',
+        'dbname' => 'open_case',
+        'user' => 'root',
+        'pass' => '',
+        'charset' => 'utf8mb4',
+    ],
+    'site' => [
+        'base_url' => 'http://localhost:8000'
+    ]
+];
